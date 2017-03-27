715ade14c7074d72f4b19c4273e249f24148990b
arielgxa <arielgxa@gmail.com>
2017-03-28 03:32:52 +1100

    floating point numbers for ex3

--- ex3.py
+++ ex3.py
@@ -12,22 +12,28 @@
 print "I will now count my chickens:"
 
 # Prints "Hens" and the number of hens
+print "Hens", 25.0 + 30.0 / 6.0
 # Prints "Roosters" and the number of roosters
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # Prints that I will be counting eggs
 print "Now I will count the eggs:"
 
 # Prints the number of eggs
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Prints a line asking if this equation is true
 print "Is it true that 3 + 2 < 5 - 7?"
 
 # Prints the answer to whether the equation is true
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # Prints a question and calculates the number to the left of the operator in the
 # previous equation
+print "What is 3 + 2?", 3.0 + 2.0
 # Prints a question and calculates the number to the right of the operator in
 # the previous equation
+print "What is 5 - 7?", 5.0 - 7.0
 
 # Prints a line of true understanding
 print "Oh, that's why it's False."
@@ -36,7 +42,10 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # Prints a question and the answer to the question
+print "Is it greater?", 5.0 > -2.0
 
 # Prints a question and the answer to the question
+print "Is it greater or equal?", 5.0 >= -2.0
 
 # Prints a question and the answer to the question
+print "Is it less or equal?", 5.0 <= -2.0
