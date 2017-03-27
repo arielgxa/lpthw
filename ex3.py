cc4e07635761eaf969f90831535e8927643b1328
arielgxa <arielgxa@gmail.com>
2017-03-28 03:32:27 +1100

    finished exercises 2 and 3

---
+++ ex3.py
@@ -0,0 +1,42 @@
+# + plus addition
+# - minus subtraction
+# / slash division
+# * asterisk multiplication
+# % percent modulus
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+# Prints that I will be counting chickens
+print "I will now count my chickens:"
+
+# Prints "Hens" and the number of hens
+# Prints "Roosters" and the number of roosters
+
+# Prints that I will be counting eggs
+print "Now I will count the eggs:"
+
+# Prints the number of eggs
+
+# Prints a line asking if this equation is true
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints the answer to whether the equation is true
+
+# Prints a question and calculates the number to the left of the operator in the
+# previous equation
+# Prints a question and calculates the number to the right of the operator in
+# the previous equation
+
+# Prints a line of true understanding
+print "Oh, that's why it's False."
+
+# Prints a line of greed
+print "How about some more."
+
+# Prints a question and the answer to the question
+
+# Prints a question and the answer to the question
+
+# Prints a question and the answer to the question
---
+++ ex3b.py
@@ -0,0 +1,32 @@
+# This script calculates the amount of money I will receive in the next Monash
+# pay cycle.
+
+# Declare hourly rates
+hourly_2089 = 131.04 # Normal without doctoral qualifications or full subject co-ordination duties
+hourly_2088 = 87.36 # Repeat without doctoral qualifications or full subject co-ordination duties
+hourly_2091 = 43.68 # Marking - Standard
+
+# Standard weekly working hours
+weekly_2089_hours = 2
+weekly_2088_hours = 2
+
+# Number of tutorial weeks completed in this pay cycle
+tutorial_weeks = 2
+
+# Accrued marking hours
+marking_hours = 0
+
+# Payments
+scholarship = 1007.62
+tutoring = (
+            tutorial_weeks * ((hourly_2089 * weekly_2089_hours) +
+            (hourly_2088 * weekly_2088_hours)) +
+            (hourly_2091 * marking_hours)
+           )
+
+# Calculate total
+amount_to_receive = scholarship + tutoring
+
+print "Scholarship:", scholarship
+print "Tutoring:", tutoring
+print "Total:", amount_to_receive
